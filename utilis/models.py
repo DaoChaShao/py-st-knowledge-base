5611c1be8bf250ad6e76fec451a751b311f7029f
wli916 <changge31@gmail.com>
2025-03-03 19:20:06 +1300

    feat: Add API key checker and Hugging Face Embedder

--- utilis/models.py
+++ utilis/models.py
@@ -1,7 +1,25 @@
 from openai import OpenAI
+from sentence_transformers import SentenceTransformer
 
 
-class Opener(object):
+def api_key_checker(api_key: str, category: str = "OpenAI") -> bool:
+    """ Check the API key format
+            - The length of an OpenAI API key is 164-digit key
+
+    :param api_key: enter the API key of the deepseek
+    :param category: the category of the API key
+    :return: True if the API key is valid
+    """
+    if api_key.startswith("sk-"):
+        if category == "OpenAI" and len(api_key) == 164:
+            return True
+        elif category == "DeepSeek" and len(api_key) == 35:
+            return True
+    return False
+
+
+class OpenAICompleter(object):
+    """ OpenAI Completer API Wrapper """
 
     def __init__(self, api_key: str, temperature: float = 0.7, top_p: float = 0.9) -> None:
         """ Initialize the OpenAI Hyperparameter Tuning API
@@ -40,7 +58,7 @@ class Opener(object):
         return completion.choices[0].message.content
 
 
-class Embedder(object):
+class OpenAIEmbedder(object):
 
     def __init__(self, api_key: str) -> None:
         """ Initialize the OpenAI Embeddings API
@@ -58,9 +76,7 @@ class Embedder(object):
         :param prompt: list: The input text to be embedded
         :return: None
         """
-        # client = OpenAI(api_key=self._config["API_KEY"], base_url="https://api.openai.com/v1")
         client = OpenAI(api_key=self._api_key, base_url="https://api.openai.com/v1")
-        # print(len(config["API_KEY"]))  # 164-digit key
 
         response = client.embeddings.create(
             input=prompt,
@@ -71,3 +87,21 @@ class Embedder(object):
         )
 
         return [item.embedding for item in response.data]
+
+
+class HuggingFaceEmbedder(object):
+
+    def __init__(self, model: str) -> None:
+        """ Initialize the Hugging Face Embeddings API
+
+        :param model: str: The model to use for the embedding
+        """
+        self._model = model
+
+    def client(self, sentences: list) -> list:
+        # Load the model from Hugging Face model hub
+        model = SentenceTransformer(self._model)
+
+        # Compute the embeddings for the sentences, the dimensionality of the embeddings is 384
+        embeddings = model.encode(sentences)
+        return embeddings.tolist()
