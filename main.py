36d863fb38d45aa4c20d2d1e38e8757b08aae711
wli916 <changge31@gmail.com>
2025-03-03 14:37:31 +1300

    feat: add sentence chunking and paragraph generation functionality

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from pprint import pprint
+from typing import List, Dict
+
+from utilis.tools import SENTENCES, Chunker
+
+
 def main():
     """ streamlit run main.py """
+    chunker = Chunker()
+
+    chunks: Dict[int, List[str]] = chunker.chunk(SENTENCES)
+    pprint(chunks)
+
+    paras = chunker.paragraph()
+    for para in paras:
+        print(para)
 
 
 if __name__ == "__main__":
