3754565f0bb42efa0327e3dc13cb22118a83f513
wli916 <changge31@gmail.com>
2025-03-03 19:20:16 +1300

    refactor: update main function to use new layout functions

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from pprint import pprint
-from typing import List, Dict
-
-from utilis.tools import SENTENCES, Chunker
+from utilis.layout import pages_configer, pages_layout
 
 
 def main():
     """ streamlit run main.py """
-    chunker = Chunker()
-
-    chunks: Dict[int, List[str]] = chunker.chunk(SENTENCES)
-    pprint(chunks)
-
-    paras = chunker.paragraph()
-    for para in paras:
-        print(para)
+    pages_configer()
+    pages_layout()
 
 
 if __name__ == "__main__":
