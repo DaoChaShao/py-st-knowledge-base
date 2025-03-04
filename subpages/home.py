49a50a0a205e0fdbf9319a87f02deee56f40a6b6
DaoChaShao <27882272@qq.com>
2025-03-04 16:47:40 +1300

    fix(home): update GitHub badge link to point to the correct repository

--- subpages/home.py
+++ subpages/home.py
@@ -6,7 +6,7 @@ divider()
 empty_message: empty = empty()
 
 with sidebar:
-    "[![Static Badge](https://img.shields.io/badge/GitHub-py--st--deepseek--api%20in%20DaoChaShao-black?style=for-the-badge&logo=github)](https://github.com/DaoChaShao/py-st-openai-api-chatbot)"
+    "[![Static Badge](https://img.shields.io/badge/GitHub-py--st--deepseek--api%20in%20DaoChaShao-black?style=for-the-badge&logo=github)](https://github.com/DaoChaShao/https://github.com/DaoChaShao/py-st-knowledge-base)"
 
 with expander("**Instructions**: Call the OpenAI model via API key.", expanded=True):
     caption("1. Clone the code from the [GitHub](https://github.com/DaoChaShao/py-st-knowledge-base) to the local.")
